f236e1d0bf04b88004c34794618f6eab993335fe
SelinJax <seljax92@gmail.com>
2025-10-08 23:54:32 +0300

    -newtask

---
+++ js-array/easy/005-stringToArray/stringToArray.js
@@ -0,0 +1,9 @@
+// Створи функцію stringToArray(str), яка приймає рядок і повертає масив слів.
+
+function stringToArray(str) {
+  return str.split(" ");
+}
+
+// Перевірка:
+
+console.log(stringToArray("JavaScript is cool")); // ["JavaScript", "is", "cool"]
