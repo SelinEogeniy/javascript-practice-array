198fcf53c8e5a07a566ee8ac199977b1f009c0be
SelinJax <seljax92@gmail.com>
2025-12-27 21:34:37 +0200

    -newTask

---
+++ js-array/normal/015-moveZeros/moveZeros.js
@@ -0,0 +1,16 @@
+// 🟡 1) moveZeros (normal)
+
+// Створи функцію moveZeros(arr), яка переносить всі нулі в кінець масиву,
+// зберігаючи порядок інших елементів.
+//! Variant 1
+function moveZeros(arr) {
+  const withoutZeros = arr.filter((num) => num !== 0);
+  const zeros = arr.filter((num) => num === 0);
+  return [...withoutZeros, ...zeros];
+}
+
+console.log(moveZeros([0, 1, 0, 3, 12]));
+// [1, 3, 12, 0, 0]
+
+console.log(moveZeros([0, 0, 1]));
+// [1, 0, 0]
